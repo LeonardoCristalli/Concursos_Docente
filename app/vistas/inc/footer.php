6f877524f1ec7a562415bc91139609c5fafd0e50
LeonardoCristalli <leocristalligithub@gmail.com>
2024-10-01 23:36:47 -0300

    CDN Fix integrity error

--- app/vistas/inc/footer.php
+++ app/vistas/inc/footer.php
@@ -17,6 +17,5 @@
         </div>
       </div>
     </footer> 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+FG4lA84HGhP8E04Bcn9P2C9aP4p2" crossorigin="anonymous"></script>
   </body>
 </html> 
--- app/vistas/inc/header.php
+++ app/vistas/inc/header.php
@@ -4,8 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php echo isset($titulo) ? $titulo : 'Inicio'; ?></title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" integrity="sha384-ECgZrxXP7IrjliTxqdAS2F23pU6zDuvD8VsaXIDbJrxFQ2rsCZslz1URybl3ErfV" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="<?php echo RUTA_URL; ?>/public/css/estilos.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
   <header class="bg-dark text-white py-3">
